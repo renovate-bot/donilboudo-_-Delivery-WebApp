f3920e08c98b974f1f5caafc7fa7ed216d93dacc
Fabrice Ilboudo <filboudo@interfacing.local>
2017-02-16 14:36:17 -0500

    change pom for creation of war file

--- src/main/java/com/ilboudofabrice/dao/ClientDAOImpl.java
+++ src/main/java/com/ilboudofabrice/dao/ClientDAOImpl.java
@@ -5,5 +5,5 @@ import com.ilboudofabrice.dao.interfaces.ClientDAO;
 /**
  * Created by filboudo on 2017-02-16.
  */
-public class ClientDAOImpl implements ClientDAO{
+public class ClientDAOImpl implements ClientDAO {
 }
