07374c80ef0d33cd01fe4cec5429e7f4ab2e3dfd
Fabrice Ilboudo <filboudo@interfacing.local>
2017-02-16 15:36:08 -0500

    add delivery servlet and home controller to project

---
+++ src/main/java/com/ilboudofabrice/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.ilboudofabrice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by filboudo on 2017-02-16.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping(path = "/")
+    public String home(ModelMap modelMap){
+        return "home";
+    }
+}
